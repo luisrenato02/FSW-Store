281d3846ad4ad6282e4232a24b6c7e7445cf8b62
Luís Renato <89888509+luisrenato02@users.noreply.github.com>
2023-11-14 10:28:19 -0300

    feat: add mouses section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -19,6 +19,14 @@ export default async function Home() {
       },
     },
   });
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses",
+      },
+    },
+  });
   return (
     <div className="flex flex-col gap-8">
       <PromoBanner
@@ -44,6 +52,9 @@ export default async function Home() {
         src="/banner-home-03.png"
         alt="Até 55% de desconto em mouses!"
       />
+
+      <SectionTitle>Mouses</SectionTitle>
+      <ProductList products={mouses} />
     </div>
   );
 }
